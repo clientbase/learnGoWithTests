b5fc88e1d3ec5d6ef178f3d114999d014c6aaab8
Tyrone Melkioty <tyrone.melkioty@aqsacom.com>
2026-04-12 20:57:42 -0400

    prepare to refactor test

---
+++ interfaces/interfacec_test.go
@@ -0,0 +1,36 @@
+package interfaces
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rect := Rectangle{10, 10}
+	got := Perimeter(rect)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rect := Rectangle{12, 6}
+		want := 72.0
+		checkArea(t, rect, want)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		want := 314.1592653589793
+		checkArea(t, circle, want)
+	})
+}
